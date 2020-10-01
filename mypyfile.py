77598ff35ed831b1ebae7c4e3cecb28d1e7b4019
aalsii <56475056+aalsii@users.noreply.github.com>
2020-10-01 22:38:24 +0530

    Update mypyfile.py

--- mypyfile.py
+++ mypyfile.py
@@ -1,3 +1,13 @@
 print("hello, this is a github repo")
 
 #This is entirely focussing on hactober fest
+
+
+num = float(input("Enter a number: "))  
+  
+if num > 0:  
+ print("{0} is a positive number".format(num))  
+elif num == 0:  
+   print("{0} is zero".format(num))   
+else:  
+   print("{0} is negative number".format(num))   
