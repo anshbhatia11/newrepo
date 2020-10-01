36047f7c2efbe019519fe86aefe7634ef5fe176d
aalsii <56475056+aalsii@users.noreply.github.com>
2020-10-01 23:00:38 +0530

    Update mypyfile.py

--- mypyfile.py
+++ mypyfile.py
@@ -42,3 +42,17 @@ elif num == 0:
    print("{0} is zero".format(num))   
 else:  
    print("{0} is negative number".format(num)) 
+    
+    
+ #new program
+year = int(input("Enter a year: "))  
+if (year % 4) == 0:  
+   if (year % 100) == 0:  
+       if (year % 400) == 0:  
+           print("{0} is a leap year".format(year))  
+       else:  
+           print("{0} is not a leap year".format(year))  
+   else:  
+       print("{0} is a leap year".format(year))  
+else:  
+   print("{0} is not a leap year".format(year))
